9583c31dffc3c4e78495697d5af1c98bdb02997d
djgoodrich <djgoodrich15@gmail.com>
2017-11-09 16:19:31 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import _ from 'lodash';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import VideoList from "./components/video-list";
+import SearchBar from "./components/searchbar";
+import VideoDetail from "./components/video-detail";
+const API_KEY = "AIzaSyAZYp3xqKiJYArFcamxUAAEc_3pdAlvFn0";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('Papaji');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        });
+      });
+  }
+  render() {
+      const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+    return (
+      <div>
+        <SearchBar onSearchTermChange={ videoSearch }/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
+// Render component to the DOM
